5b9876937fc92b09040442391422809f4947f323
Olasupotomiwa <tomiwaolasupo@gmail.com>
2024-09-21 10:46:45 +0100

    Added placeholder

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This site is under construction
         </a>
       </header>
     </div>
